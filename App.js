40507013583ad57f58fd556221aab75d775580cb
CapelliC <cc.carlo.cap@gmail.com>
2020-04-21 11:20:31 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,171 @@
+import * as React from 'react';
+import { Text, Image, View, StyleSheet, Button
+	, Alert, ScrollView // CC
+} from 'react-native';
+import Constants from 'expo-constants';
+import * as Print from 'expo-print';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+//import { Card } from 'react-native-paper';
+
+// CC
+import { Asset } from 'expo-asset'
+import * as FileSystem from 'expo-file-system'
+
+export default class App extends React.Component {
+
+  state = {
+    image: null,
+    
+    required: null,
+    local_doc: null,
+    local_cache: null,
+  }
+
+  componentDidMount() {
+    this.getPermissionAsync();
+        
+    const required = Asset.fromModule(require('./components/AssetExample/test.png'))
+    console.log('required', required)
+    this.setState({required})
+    
+    required.downloadAsync().then(() => {
+console.log('required.localUri', required.localUri)
+
+			const local_doc = FileSystem.documentDirectory+'local_doc.png'
+			FileSystem.copyAsync({
+				from: required.localUri,
+				to: local_doc
+			}).then(() => {
+				console.log('local_doc', local_doc)
+				this.setState({local_doc})
+			})
+			
+			const local_cache = FileSystem.cacheDirectory+'local_cache.png'
+			FileSystem.copyAsync({
+				from: required.localUri,
+				to: local_cache
+			}).then(() => {
+				console.log('local_cache', local_cache)
+				this.setState({local_cache})
+			})
+    })
+    .catch(e => {
+      Alert.alert('exception', JSON.stringify(e))
+    })
+		
+  }
+
+  getPermissionAsync = async () => {
+    //if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    //}
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+
+    console.log('result',result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result });
+    }
+  }
+  
+  _printPdf() {
+    const { image, required, local_doc, local_cache } = this.state
+    
+console.log('print images:', this.state)
+		Print.printAsync({
+			html: `
+<html>
+<head>
+   <meta name="viewport" content ="width=device-width,initial-scale=1,user-scalable=yes" />
+</head>
+<body>
+	<div style="height:100%; margin: 0px">
+
+		<h1>local image from photo:</h1>
+		<img src="${image.uri}" width="300" height="300" />
+
+		<h1>from local doc:</h1>
+		<img src="${local_doc}" width="300" height="300" />
+
+		<h1>from local cache:</h1>
+		<img src="${local_cache}" width="300" height="300" />
+
+		<h1>required:</h1>
+		<img src="${required.uri}" width="300" height="300" />
+
+		<h1>required localUri:</h1>
+		<img src="${required.localUri}" width="300" height="300" />
+
+		<h1>remote image</h1>
+		<img src="https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" style="border:3px;" width="300" height="300" />
+
+	</div>
+</body>
+</html>`
+    })
+  }
+  render() {
+    const { image, required, local_doc, local_cache } = this.state;
+console.log('images:', image, required, local_doc, local_cache)
+    return (
+      <ScrollView style={styles.container}>
+
+				{image === null
+					&& <Button title="First, pick an image" onPress={this._pickImage} />
+				}
+				{image &&
+					<View>
+						<Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
+						<Button title="Then, print" onPress={()=>this._printPdf()} />
+					</View>
+				}
+				
+				<Text>local doc</Text>
+				{local_doc &&
+					<Image source={{ uri: local_doc }} style={{ width: 200, height: 200 }} />}
+					
+				<Text>local cache</Text>
+				{local_cache &&
+					<Image source={{ uri: local_cache }} style={{ width: 200, height: 200 }} />}
+
+				<Text>required</Text>
+				{required &&
+					<Image source={{ uri: required.uri }} style={{ width: 200, height: 200 }} />}
+
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # expo-print-test-pdf-file-uri-missing-ios
 highlight the different treatment that file:// uri images get on iOS vs Android when using Print.printAsync
+
+###
+
+follow up of https://github.com/expo/expo/issues/7435, as per https://github.com/expo/expo/issues/7435#issuecomment-613353712
+
+###
+
+have removed the dependencies
+
+    "react-native-paper": "^3.8.0",
+    "react-native-web": "~0.11.7",
+
+and added a components/AssetExample/test.png that gets copied in FileSystem cache & documents folder at startup
+and these dependencies
+
+    "expo-file-system": "~8.1.0",
+    "expo-asset": "~8.1.3"
+
+###
+
+The iOS PDF did showed the local images at least til SDK ver.31,
+after I discovered that passing the (undocumented?) option
+*baseUrl* to Print.printAsync allowed the iOS print engine to load the image files.
+
+Then I had to upgrade to the latest SDK, to follow up with maintenance duties,
+and the PDF doesn't show any kind of file:// URIs
+
